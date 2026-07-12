bd2950e54ae6143d734cacfd012b048866456ab3
vishesh-sahu-12 <visheshsahu2010@gmail.com>
2026-07-12 18:13:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("🚀 Anshul Kirana Premium Website Loaded");
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
